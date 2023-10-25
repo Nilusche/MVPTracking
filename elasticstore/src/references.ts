11847248a63942b4c6c41096d194e8fb53546385
majd bousaad <bousaadmajd@gmail.com>
2023-10-25 19:05:36 +0200

    Adding Documents configurations in references.ts

--- elasticstore/src/references.ts
+++ elasticstore/src/references.ts
@@ -3,9 +3,34 @@ import { Reference } from "./types"
 // Records should be added here to be indexed / made searchable
 const references: Array<Reference> = [
   {
-    collection: "example",
-    index: "example",
-    include: ["name", "email", "biography", "updatedAt", "createdAt"],
+    collection: "users",
+    index: "users",
+    /* 
+    mappings: {
+      location: {
+        type: "geo_point" // elasticsearch's definition of a geopoint
+      }
+    },
+    transform: (data, parent) => ({
+      ...data,
+      location: `${data.location._latitude},${data.location._longitude}` // transform from firestore's geopoint to elasticsearch's
+    })
+    */
+  },
+  {
+    collection: "Coach",
+    index: "Coach",
+    /* 
+    mappings: {
+      location: {
+        type: "geo_point" // elasticsearch's definition of a geopoint
+      }
+    },
+    transform: (data, parent) => ({
+      ...data,
+      location: `${data.location._latitude},${data.location._longitude}` // transform from firestore's geopoint to elasticsearch's
+    })
+    */
   },
 ]
 
