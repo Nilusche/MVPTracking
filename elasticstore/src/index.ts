c52af3963b0f38546c5b73dd517ba46a26b8ab37
majdbousaad <34665497+majdbousaad@users.noreply.github.com>
2024-01-06 14:51:17 +0100

    Delete elasticstore directory

--- elasticstore/src/index.ts
+++
@@ -1,9 +0,0 @@
-import Elasticstore from "./elasticstore"
-import 'source-map-support/register'
-
-async function main() {
-  const elasticstore = new Elasticstore()
-  elasticstore.init()
-}
-
-main()
